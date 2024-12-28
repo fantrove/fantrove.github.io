fb33df547206aee2d91a712df84cf78b882a9469
Jeffy2600II <nontakorn2600@gmail.com>
2024-12-28 09:17:17 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 function navigateTo(page) {
-    // ล้าง :hover ทุกองค์ประกอบ
-    document.querySelectorAll('*').forEach(element => {
-        element.classList.remove('hover');
-    });
-
-    // เปลี่ยนหน้า
-    window.location.href = page;
+ window.location.href = page;
 }
-
-window.onbeforeunload = function() {
-    // ล้าง :hover ทุกองค์ประกอบเมื่อออกจากหน้า
-    document.querySelectorAll('*').forEach(element => {
-        element.classList.remove('hover');
-    });
-};
