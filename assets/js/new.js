174981b37ad6fc865e2c52712623dba097ebded5
Jeffy2600II <nontakorn2600@gmail.com>
2025-03-20 09:18:15 +0700

    update

--- assets/js/new.js
+++
@@ -1,6 +0,0 @@
-fetch('./assets/json/version.json')
- .then(response => response.json())
- .then(data => {
-  document.getElementById('title').innerText = `${data.version}`;
- })
- .catch(error => console.error('Error loading version:', error));
